e145d54bb1dc8c011dbe70534c8a2f5eeab388ea
Thiago Gaspar <thiagogasparr@gmail.com>
2021-01-20 17:47:06 -0300

    Finalizando formulário HTML. Desafio 4-3

---
+++ teachers.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const data = require('./data.json')
+
+exports.post = function(req, res) {
+    
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        if (req.body[key] == "") {
+            return res.send("Please, fill all fields!")
+        }
+    }
+
+    data.teachers.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) { 
+            return res.send("Write file error!")
+        }
+
+            return res.redirect("/teachers")
+    })
+}
